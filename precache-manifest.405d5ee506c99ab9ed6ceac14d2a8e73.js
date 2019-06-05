440667c4172aa91f87bb1aba56d70ae74b4fd061
Leandro <leandro.frigerio@devugees.org>
2019-06-05 14:40:28 +0200

    Updates

---
+++ precache-manifest.405d5ee506c99ab9ed6ceac14d2a8e73.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce82ecfd453ebe62bdc616168ca48f30",
+    "url": "/react-dog-ceo/index.html"
+  },
+  {
+    "revision": "75dc665f2e932f83acda",
+    "url": "/react-dog-ceo/static/css/main.98c73720.chunk.css"
+  },
+  {
+    "revision": "53ab40e5a11de7f85c58",
+    "url": "/react-dog-ceo/static/js/2.f0801e53.chunk.js"
+  },
+  {
+    "revision": "75dc665f2e932f83acda",
+    "url": "/react-dog-ceo/static/js/main.03551f41.chunk.js"
+  },
+  {
+    "revision": "fb7da985cc990321adeb",
+    "url": "/react-dog-ceo/static/js/runtime~main.7cb123e1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-dog-ceo/static/media/logo.5d5d9eef.svg"
+  }
+]);
